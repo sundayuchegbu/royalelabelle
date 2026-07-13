15cdd841bc79c1f6f3399469650d173284f986cc
sundayuchegbu <sundayuchegbu10@gmail.com>
2026-07-13 21:30:32 +0100

    fix deployment

--- backend/index.js
+++ backend/index.js
@@ -41,9 +41,18 @@ console.log(
 // CORS
 // --------------------------------------------------
 
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+//     credentials: true,
+//   }),
+// );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [
+      "https://royalelabelle.netlify.app",
+      "http://localhost:3000", // Keep for local development
+    ],
     credentials: true,
   }),
 );
